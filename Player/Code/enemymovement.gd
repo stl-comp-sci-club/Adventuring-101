c9efa4b75fccba5dd9956ebd63a571409dc5ff8b
littlefacemitt <81489461+littlefacemitt@users.noreply.github.com>
2022-02-17 15:26:49 -0500

    the enemy moves

---
+++ Player/Code/enemymovement.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+export (int) var speed = 100
+
+var velocity = Vector2()
+onready var player : KinematicBody2D = get_node("/root/World/Player")
+
+
+func move():
+	velocity = Vector2()
+	velocity = position.direction_to(player.position)
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	move()
+	velocity = move_and_slide(velocity)
+	
