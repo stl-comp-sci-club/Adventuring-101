aa83703996473619fb51e7714a343c406011da21
MrMan314 <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@github.com>
2022-08-13 22:18:42 -0400

    killing elijah

--- Test_Enemy_AI.gd
+++ Test_Enemy_AI.gd
@@ -8,6 +8,8 @@ var player_in_detection_area = false
 
 var health = 100.0
 
+var last_direction = Vector2(0,1)
+
 var rng = RandomNumberGenerator.new()
 
 var stunned = false
@@ -20,8 +22,6 @@ func _input(event):
 	if Input.is_action_just_pressed("Interact"):
 		print(player.position)
 		print(position)
-
-
 
 var velocity = Vector2.ZERO
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -49,8 +49,7 @@ func _process(delta):
 		velocity = position.direction_to(player.position) * -1
 		velocity *= 500
 		move_and_slide(velocity)
-		
-
+	animate(velocity)
 
 func _on_Detection_Area_body_entered(body):
 	if body == player:
@@ -98,3 +97,25 @@ func _on_Hitbox_area_entered(area):
 		print(position.direction_to(player.position) * -1)
 		print(velocity)
 		move_and_slide(velocity)
+
+
+func get_animation_direction(direction: Vector2):
+	var norm_direction = direction.normalized()
+	if norm_direction.y >= 0.707:
+		return "down"
+	elif norm_direction.y <= -0.707:
+		return "up"
+	elif norm_direction.x <= -0.707:
+		return "left"
+	elif norm_direction.x >= 0.707:
+		return "right"
+	return "down"
+
+func animate(direction: Vector2):
+	if direction != Vector2.ZERO:
+		last_direction = direction
+		var d = get_animation_direction(last_direction)
+		$enemy.play("walk_"+d)
+	else:
+		var d = get_animation_direction(last_direction)
+		$enemy.play(d+"_resting")
