59ce1f9071024c2be70f0dbf945697c1b7188537
Hiatus <matiasrivas3106@gmail.com>
2022-08-23 12:16:32 -0400

    Added time stuff in global
    If you want to make it better add the ability to pause time and resume with a bool or something.

--- Scenes/DayNightCycle.gd
+++ Scenes/DayNightCycle.gd
@@ -6,14 +6,21 @@ extends CanvasModulate
 # var b = "text"
 const nightColor = Color("#281E56")
 const dayColor = Color("#F4F1D9")
-const timeConst = 0.1
-var time = 0
-
+var calc = 0
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
 	pass
 
 func _process(delta:float):
-	self.time += delta * timeConst
-	self.color = nightColor.linear_interpolate(dayColor, abs(sin(time)))
+	# Dividing by twelve makes each half of the day 12 minutes long. 
+	# Imagine a circle with circumfrence 2PI, and the TIME variable as each minute 
+	# if you do sin(PI * TIME) the speed at period of the sine wave is 1 
+	# If you want to see what it does just go on desmos and put sin(PI * x) and play around with it 
+	
+	if sin(PI/12 * Global.TIME) < 0:
+		calc = sin(PI/12 * Global.TIME)/20
+	else: 
+		calc = sin(PI/12 * Global.TIME)
+		
+	self.color = nightColor.linear_interpolate(dayColor, calc)
